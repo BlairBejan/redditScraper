d1b0381cf34f423168e1178790771138266e44ae
bbejan <Blair.bejan@gmail.com>
2024-09-28 16:07:23 -0500

    split up some useful functions to separate files for reusability

--- Scripts/redditScraper.py
+++ Scripts/redditScraper.py
@@ -5,14 +5,10 @@ from datetime import datetime, timezone
 import json
 import os
 from dbfunctions import save_posts
+from utils import read_config, get_soup
 
-print("Environment variables:\n", os.environ)
 #import data from config file
-config_path = os.path.join(os.path.dirname(__file__),
-                           '../Config/config.json')
-with open(config_path, 'r') as config_file:
-    config = json.load(config_file)
-
+config = read_config()
 url = config['url']
 user_url = config['user_url']
 headers = config['headers']
@@ -21,7 +17,7 @@ last_run = datetime.strptime(last_runtest, '%Y-%m-%d %H:%M:%S%z')
 
 
 
-
+'''
 def make_request(url, headers):
     try:
         response = requests.get(url, headers=headers)
@@ -30,40 +26,44 @@ def make_request(url, headers):
     except requests.exceptions.RequestException as error:
         print(f"Error making request to {url}: {error}")
         return None
-    
-def scrape_data(url, headers, last_run):
-    content = make_request(url, headers)
+'''
+
+'''    content = make_request(url, headers)
     if content:
         soup = BeautifulSoup(content, 'html.parser')
-        posts = soup.find_all('div', class_='thing')
-        for post in posts:
-            #this is a check to see if a post is an ad
-            comments_element = post.find('a', class_='bylink comments may-blank')
-            if comments_element is not None:
-                time_tag = post.find('time')
-                post_time = time_tag['datetime']
-                formatted_post_time = datetime.fromisoformat(post_time)
-                print("post time")
-                print(formatted_post_time)
-                if formatted_post_time > last_run:
-                    data = {
-                        "reddit_post_id": post['data-fullname'].split('_')[-1],
-                        "title": post.find('p', class_='title').a.text,
-                        "author_name": post.find('p', class_='tagline').a.text,
-                        "upvotes": post.find('div', class_='score unvoted').text,
-                        "comments_url": urljoin(url, comments_element['href']),
-                        "author_url": urljoin(user_url, post.find('p', class_='tagline').a.text)
-                        }
-                    print(data)
-                    save_posts(data)
-                else: return
+'''
+def scrape_data(url, headers, last_run):
+    soup = get_soup(url, headers)
+    posts = soup.find_all('div', class_='thing')
+    for post in posts:
+        #this is a check to see if a post is an ad
+        comments_element = post.find('a', class_='bylink comments may-blank')
+        if comments_element is not None:
+            time_tag = post.find('time')
+            post_time = time_tag['datetime']
+            formatted_post_time = datetime.fromisoformat(post_time)
+            print("post time")
+            print(formatted_post_time)
+            if formatted_post_time > last_run:
+                data = {
+                    "reddit_post_id": post['data-fullname'].split('_')[-1],
+                    "title": post.find('p', class_='title').a.text,
+                    "author_name": post.find('p', class_='tagline').a.text,
+                    "upvotes": post.find('div', class_='score unvoted').text,
+                    "comments_url": urljoin(url, comments_element['href']),
+                    "author_url": urljoin(user_url, post.find('p', class_='tagline').a.text)
+                    }
+                print(data)
+                save_posts(data)
+            else: return
 
     
 scrape_data(url, headers, last_run)
 
 now = datetime.now(timezone.utc)
 
+# write another function in utils to handle saving json dota, maybe have it accept any amount of inputs that you want to change
 config['last_run'] = now.strftime('%Y-%m-%d %H:%M:%S%z')
-
+config_path = os.path.join(os.path.dirname(__file__), '../Config/config.json')
 with open(config_path, 'w') as config_file:
     json.dump(config, config_file, indent=4)
--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,16 +1,30 @@
 import os
 from datetime import datetime
 import json
+import requests
+from bs4 import BeautifulSoup
 
 def read_config():
     config_path = os.path.join(os.path.dirname(__file__), '../Config/config.json')
     
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
+    return(config)
 
-    url = config['url']
-    user_url = config['user_url']
-    headers = config['headers']
-    last_runtest = config['last_run']
-    last_run = datetime.strptime(last_runtest, '%Y-%m-%d %H:%M:%S%z')
-    return(url, user_url, headers, last_run)
+def get_soup(url, headers):
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        print (soup)
+        return soup
+    except requests.exceptions.RequestException as error:
+        print(f"Error making request to {url}: {error}")
+        return None
+
+'''
+get_soup("https://old.reddit.com/r/uspolitics/new/", {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    })
+read_config()
+'''
